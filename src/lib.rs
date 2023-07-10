b972605e89b752912b4ecaae9ef00a2a1ed2eca5
Yota Toyama <raviqqe@gmail.com>
2023-07-10 18:33:04 +1000

    Update (#2)

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod build;
+mod document;
+mod format;
+mod utils;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use build::*;
+pub use document::*;
+pub use format::*;
+pub use utils::*;
