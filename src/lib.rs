6552deee89cf9bbe18780d74715cd9095a4c9605
Yota Toyama <raviqqe@gmail.com>
2023-07-10 18:22:40 +1000

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod context;
+mod ir;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use context::Context;
+pub use ir::{build::*, count_lines, is_broken, Document};
