804b334f65d346551d2c672af002ebd2b264e801
Yota Toyama <raviqqe@gmail.com>
2023-07-13 16:53:32 +1000

    Fix

--- src/utility.rs
+++ src/utility.rs
@@ -24,3 +24,28 @@ pub fn count_lines(document: &Document) -> usize {
         Document::LineSuffix(_) | Document::String(_) => 0,
     }
 }
+
+pub fn is_empty(document: &Document) -> bool {
+    match document {
+        Document::Break(_, document) => is_empty(document),
+        Document::Indent(document) => is_empty(document),
+        Document::Sequence(documents) => documents.iter().all(is_empty),
+        Document::String(string) => string.is_empty(),
+        Document::Line | Document::LineSuffix(_) => false,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{line, line_suffix};
+
+    #[test]
+    fn check_empty() {
+        assert!(is_empty(&"".into()));
+        assert!(!is_empty(&"foo".into()));
+        assert!(!is_empty(&line()));
+        assert!(is_empty(&line_suffix("")));
+        assert!(!is_empty(&line_suffix("foo")));
+    }
+}
