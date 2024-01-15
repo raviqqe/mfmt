c902268fbc735b85290fb69db5de3ddca7f0cac7
Yota Toyama <raviqqe@gmail.com>
2024-01-15 22:50:24 +0900

    Fix

--- src/options.rs
+++ src/options.rs
@@ -1,7 +1,19 @@
+#[derive(Clone, Copy, Debug, Default)]
 pub struct FormatOptions {
-    space: ' ',
-    indent: 2,
+    pub(crate) indent: usize,
+    pub(crate) space: char,
 }
 
 impl FormatOptions {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn indent(self, space: char) -> Self {
+        Self { space, ..self }
+    }
+
+    pub fn space(self, space: char) -> Self {
+        Self { space, ..self }
+    }
 }
