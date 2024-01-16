f6298808a867aeb5d7f5ff73861016206df439dd
Yota Toyama <raviqqe@gmail.com>
2024-01-16 22:41:52 +0900

    Fix

--- src/build/builder.rs
+++ src/build/builder.rs
@@ -1,20 +1,24 @@
 use super::{flatten, indent, line_suffix, r#break, sequence, Document};
 use alloc::{alloc::Allocator, boxed::Box, str, vec::Vec};
 
+/// Document builder.
 #[derive(Clone, Debug)]
 pub struct Builder<A: Allocator> {
     allocator: A,
 }
 
 impl<'a, A: Allocator + Clone + 'a> Builder<A> {
+    /// Creates a document builder.
     pub fn new(allocator: A) -> Self {
         Self { allocator }
     }
 
+    /// Returns an allocator.
     pub fn allocator(&self) -> &A {
         &self.allocator
     }
 
+    /// Creates a sequence of documents.
     pub fn sequence(
         &self,
         values: impl IntoIterator<Item = impl Into<Document<'a>>>,
@@ -22,30 +26,37 @@ impl<'a, A: Allocator + Clone + 'a> Builder<A> {
         sequence(self.allocate_slice(values.into_iter().map(Into::into)))
     }
 
+    /// Flattens a document.
     pub fn flatten(&self, value: impl Into<Document<'a>>) -> Document<'a> {
         flatten(self.allocate(value.into()))
     }
 
+    /// Indents a document by a level.
     pub fn indent(&self, value: impl Into<Document<'a>>) -> Document<'a> {
         indent(self.allocate(value.into()))
     }
 
+    /// Breaks a document into multiple lines.
     pub fn r#break(&self, value: impl Into<Document<'a>>) -> Document<'a> {
         r#break(self.allocate(value.into()))
     }
 
+    /// Creates a concatenation of strings.
     pub fn strings<'b>(&self, values: impl IntoIterator<Item = &'b str>) -> Document<'a> {
         self.allocate_str(values).into()
     }
 
+    /// Creates a set of line suffixes.
     pub fn line_suffixes<'b>(&self, values: impl IntoIterator<Item = &'b str>) -> Document<'a> {
         line_suffix(self.allocate_str(values))
     }
 
+    /// Allocates a value.
     pub fn allocate<T>(&self, value: T) -> &'a T {
         Box::leak(Box::new_in(value, self.allocator.clone()))
     }
 
+    /// Allocates a slice.
     pub fn allocate_slice<T>(&self, values: impl IntoIterator<Item = T>) -> &'a [T] {
         let mut vec = Vec::new_in(self.allocator.clone());
 
@@ -54,6 +65,7 @@ impl<'a, A: Allocator + Clone + 'a> Builder<A> {
         Vec::leak(vec)
     }
 
+    /// Allocates a string.
     pub fn allocate_str<'b>(&self, values: impl IntoIterator<Item = &'b str>) -> &'a str {
         let mut vec = Vec::new_in(self.allocator.clone());
 
